a5dca37566c97e2926b264c044162c29a52ca480
Abhishek Kumawat <112508513+abhishek-kumawat-1@users.noreply.github.com>
2025-01-11 01:20:08 +0530

    Update seo_title_metadesc.py

--- seo_title_metadesc.py
+++ seo_title_metadesc.py
@@ -5,9 +5,11 @@ import openpyxl
 #LinkedIn Profile
 with st.sidebar:
        st.components.v1.html(
-             """ <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-             <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="abhishek-kumawat-0530671ba" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/abhishek-kumawat-0530671ba?trk=profile-badge">Abhishek Kumawat</a></div>
-              ,height=310"""
+             """ 
+<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="abhishek-kumawat-0530671ba" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/abhishek-kumawat-0530671ba?trk=profile-badge"></a></div>
+
+              """
               
        )
 # Streamlit app title
